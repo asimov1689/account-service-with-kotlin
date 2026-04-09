0b1cbddd3b932381347cb7236ab247b221ee565a
Oliver Jaramillo <asimov16.89@gmail.com>
2026-04-09 17:45:05 +0200

    commit all component versions for initial curl tests

--- Account Service (Kotlin)/task/src/account/exception/GlobalExceptionHandler.kt
+++ Account Service (Kotlin)/task/src/account/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package account.exception
 import account.dto.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.time.LocalDateTime
@@ -10,6 +11,23 @@ import jakarta.servlet.http.HttpServletRequest
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun handleMethodNotAllowed(
+        ex: HttpRequestMethodNotSupportedException,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+
+        val body = ErrorResponse(
+            timestamp = LocalDateTime.now(),
+            status = HttpStatus.METHOD_NOT_ALLOWED.value(),
+            error = "Method Not Allowed",
+            message = "Method ${ex.method} is not supported for this endpoint",
+            path = request.requestURI
+        )
+
+        return ResponseEntity(body, HttpStatus.METHOD_NOT_ALLOWED)
+    }
 
     @ExceptionHandler(Exception::class)
     fun handleGenericError(
